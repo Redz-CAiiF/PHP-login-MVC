a9767fa03f2ae28ed1911982ac038b62d79074ff
Redz CAiiF <Redz-CAiiF@users.noreply.github.com>
2019-02-23 21:57:04 +0100

    components update

---
+++ Components/DataTables-1.10.18/js/datatableJS.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('#addressTable').DataTable( {
+        /*"language": {
+            "zeroRecords": "Nessun dato trovato...",
+            "search": "Cerca:",
+            "infoEmpty": "Nessuna riga disponibile"
+        },
+        paging: true,
+        searching: true,
+        info: true,*/
+        /*columnDefs: [ {
+            orderable: false,
+            className: 'select-checkbox',
+            targets:   0
+        } ],
+        select: {
+            style:    'os',
+            selector: 'td:first-child'
+        },
+        order: [[ 1, 'asc' ]]*/
+        /*select: true*/
+    } );
+} );
