5e6f23d838a3d0e309f9c76c813281effbb587a1
Redz CAiiF <Redz-CAiiF@users.noreply.github.com>
2019-05-05 16:01:00 +0200

    component updated

--- Components/customSelect/js/customSelect.js
+++ Components/customSelect/js/customSelect.js
@@ -1,8 +1,13 @@
-jQuery.document_ready (function() {
-
+function bind(){
+    var index = 0;
     $('.md-select').on('click', function() {
-        $(this).toggleClass('active');
-        $('.block').toggleClass('visible');
+        if(index===0){
+            $(this).addClass('active');
+            $('.block').addClass('visible');
+            index++;
+        }else{
+            index=0;
+        }
     });
 
     $('.block').on('click', function() {
@@ -16,6 +21,19 @@ jQuery.document_ready (function() {
         $(this).addClass('active');
         $(this).parent().siblings("label").text(v);
         $(this).parent().parent().siblings("input").attr('value', v);
+        //rimuovo la classe active e visible
+        $('.md-select').removeClass('active');
+        $('.block').removeClass('visible');
     });
+}
+function unbind(){
+    $('.md-select').unbind();
+    $('.block').unbind();
+    $('.md-select ul li').unbind();
+}
 
+$(document).ready (bind);
+$(document).ajaxComplete (function(){
+    unbind();
+    bind();
 });
